8def7482c8ed4603f2a499ef0b20f36834a13160
nramirezhelvis <ramirezherreranoeelvis@gmail.com>
2024-09-18 15:57:59 -0500

    seccion de populares

---
+++ model/Novedad.js
@@ -0,0 +1,9 @@
+export class Novedad {
+        constructor(id, portada, name, context, date) {
+                this.id = id;
+                this.portada = portada;
+                this.name = name;
+                this.context = context;
+                this.date = date;
+        }
+}
---
+++ model/Popular.js
@@ -0,0 +1,14 @@
+export class Popular {
+        constructor(id, url, author, name, score, stars, votes, price, pricePrevous, portada) {
+                this.id = id;
+                this.url = url;
+                this.author = author;
+                this.name = name;
+                this.score = score;
+                this.stars = stars;
+                this.votes = votes;
+                this.price = price;
+                this.pricePrevous = pricePrevous;
+                this.portada = portada;
+        }
+}
